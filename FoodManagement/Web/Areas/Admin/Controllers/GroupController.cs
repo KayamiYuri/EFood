d6e666af35041b49d28d2d27e7c1f67d24d73548
Yuri Kayumi <phucnn.22th@sv.daihoclongan.edu.vn>
2024-06-30 16:42:27 +0700

    Lab 3: Group List

---
+++ FoodManagement/Web/Areas/Admin/Controllers/GroupController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Areas.Admin.Models;
+using Web.Models.EF;
+using System.Linq.Dynamic.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class GroupController : Controller
+    {
+        private readonly FoodContext _dbcontext;
+
+        public GroupController(FoodContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetList(JDatatable model)
+        {
+            var items = (from i in _dbcontext.Groups select i);
+            int recordsTotal = 0;
+            if (!string.IsNullOrEmpty(model.columns[model.order[0].column].name) && !string.IsNullOrEmpty(model.order[0].dir))
+            {
+                items = items.OrderBy(model.columns[model.order[0].column].name + ' ' + model.order[0].dir);
+            }
+            if (!string.IsNullOrEmpty(model.search.value))
+            {
+                items = items.Where(i => i.Name.Contains(model.search.value));
+            }
+            recordsTotal = items.Count();
+
+            var data = await items.Select(i => new
+            {
+                i.Id,
+                i.Name
+            }).Skip(model.start).Take(model.length).ToListAsync();
+
+            var jsonData = new { draw = model.draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = data };
+            return Ok(jsonData);
+        }
+    }
+}
